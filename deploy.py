59e10532d452f3f695c101f733fec0566339c73d
youssef maged <youssef.maged237@gmail.com>
2026-05-12 15:07:59 +0300

    Update ci-cd pipeline

--- deploy.py
+++ deploy.py
@@ -5,6 +5,10 @@ from lightning_sdk.deployment import Deployment
 deployment = Deployment(name="home-credit-risk", teamspace=os.getenv("TEAMSPACE"), user=os.getenv("LIGHTNING_USERNAME"))
 
 deployment.start(
-    image=f"{os.getenv('DOCKERHUB_USERNAME')}/{os.getenv('IMAGE_NAME')}:{os.getenv('CI_COMMIT_SHORT_SHA')}",  # Ollama Docker image
+    image=f"{os.getenv('DOCKERHUB_USERNAME')}/{os.getenv('IMAGE_NAME')}:{os.getenv('CI_COMMIT_SHORT_SHA')}",
     ports=[8000],
+    env={
+        "DAGSHUB_TOKEN": os.getenv("DAGSHUB_TOKEN"),
+        "DAGSHUB_USERNAME": os.getenv("DAGSHUB_USERNAME"),
+    }
 )
